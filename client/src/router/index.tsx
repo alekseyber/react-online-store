7005d4396601416b4a1d956afeb2204b1e967468
alekseyber <al_berestov@mail.ru>
2021-11-29 02:33:19 +0300

    Update router

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, Suspense, lazy } from "react";
+import { ReactNode, Suspense, lazy, LazyExoticComponent, FC } from "react";
 import { Routes, Route } from "react-router-dom";
 import PageSceleton from "../components/skeletons/PageSceleton";
 import ProductListSceleton from "../components/skeletons/ProductListSceleton";
@@ -37,6 +37,47 @@ const CommentPage = lazy(() => import("../pages/comment/CommentPage"));
 const IndexNewsPage = lazy(() => import("../pages/news/IndexNewsPage"));
 const NewsPage = lazy(() => import("../pages/news/NewsPage"));
 
+interface IRoute {
+  path: string;
+  Element: LazyExoticComponent<FC<{}>>;
+}
+
+export enum RouteNames {
+  MAIN_PAGE = "/",
+  CATEGORY_PAGE = "/category/:alias",
+  PRODUCT_PAGE = "/product/:alias",
+  CART_PAGE = "/cart",
+  DELIVERY_PAGE = "/delivery",
+  ORDER_DONE_PAGE = "/order/done",
+  ORDER_PAGE = "/order/:id",
+  ADD_CART_PAGE = "/addcart/:id",
+  PAGE_PAGE = "/page/:alias",
+  RETURN_PRODUCT_PAGE = "/returnproduct",
+  SEARCH_PAGE = "/search",
+  COMMENT_PAGE = "/comment",
+  NEWS_PAGE = "/news/:alias",
+  INDEX_NEWS_PAGE = "/news",
+  PAGE_FOUND_PAGE = "*",
+}
+
+const appRoutes: IRoute[] = [
+  { path: RouteNames.MAIN_PAGE, Element: MainPage },
+  { path: RouteNames.CATEGORY_PAGE, Element: CategoryPage },
+  { path: RouteNames.PRODUCT_PAGE, Element: ProductPage },
+  { path: RouteNames.CART_PAGE, Element: CartPage },
+  { path: RouteNames.DELIVERY_PAGE, Element: DeliveryPage },
+  { path: RouteNames.ORDER_DONE_PAGE, Element: OrderDonePage },
+  { path: RouteNames.ORDER_PAGE, Element: OrderPage },
+  { path: RouteNames.ADD_CART_PAGE, Element: AddCartPage },
+  { path: RouteNames.PAGE_PAGE, Element: PagePage },
+  { path: RouteNames.RETURN_PRODUCT_PAGE, Element: ReturnProductPage },
+  { path: RouteNames.SEARCH_PAGE, Element: SearchPage },
+  { path: RouteNames.COMMENT_PAGE, Element: CommentPage },
+  { path: RouteNames.NEWS_PAGE, Element: NewsPage },
+  { path: RouteNames.INDEX_NEWS_PAGE, Element: IndexNewsPage },
+  { path: RouteNames.PAGE_FOUND_PAGE, Element: PageFoundPage },
+];
+
 export const useRoutes = (): ReactNode => {
   return (
     <Suspense
@@ -47,22 +88,26 @@ export const useRoutes = (): ReactNode => {
       }
     >
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/category/:alias" element={<CategoryPage />} />
-        <Route path="/product/:alias" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/delivery" element={<DeliveryPage />} />
-        <Route path="/order/done" element={<OrderDonePage />} />
-        <Route path="/order/:id" element={<OrderPage />} />
-        <Route path="/addcart/:id" element={<AddCartPage />} />
-        <Route path="/page/:alias" element={<PagePage />} />
-        <Route path="/returnproduct" element={<ReturnProductPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/comment" element={<CommentPage />} />
-        <Route path="/news/:alias" element={<NewsPage />} />
-        <Route path="/news" element={<IndexNewsPage />} />
-        <Route path="*" element={<PageFoundPage />} />
+        {appRoutes.map(({ path, Element }) => (
+          <Route path={path} element={<Element />} key={path} />
+        ))}
       </Routes>
     </Suspense>
   );
 };
+
+// <Route path="/" element={<MainPage />} />
+// <Route path="/category/:alias" element={<CategoryPage />} />
+// <Route path="/product/:alias" element={<ProductPage />} />
+// <Route path="/cart" element={<CartPage />} />
+// <Route path="/delivery" element={<DeliveryPage />} />
+// <Route path="/order/done" element={<OrderDonePage />} />
+// <Route path="/order/:id" element={<OrderPage />} />
+// <Route path="/addcart/:id" element={<AddCartPage />} />
+// <Route path="/page/:alias" element={<PagePage />} />
+// <Route path="/returnproduct" element={<ReturnProductPage />} />
+// <Route path="/search" element={<SearchPage />} />
+// <Route path="/comment" element={<CommentPage />} />
+// <Route path="/news/:alias" element={<NewsPage />} />
+// <Route path="/news" element={<IndexNewsPage />} />
+// <Route path="*" element={<PageFoundPage />} />
